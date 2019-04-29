cf73e57e5aa44532bd3c8bef0a7db1b4f70a5ccb
Rene Meza <rene.meza125@gmail.com>
2019-04-29 16:42:06 -0600

    Reformatting file indent

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,17 @@ COPY_SCRIPTS_ONLY=${COPY_SCRIPTS_ONLY:-"false"}
 COPY_SCRIPTS_DIR=${COPY_SCRIPTS_DIR:-"sls-deploy-action"}
 
 copy_files() {
-    echo "### Copying scripts to ${HOME}/sls-scripts"
-    mkdir -p "${HOME}/${COPY_SCRIPTS_DIR}"
-    cp -R /usr/local/bin/* "${HOME}/${COPY_SCRIPTS_DIR}/."
+  echo "### Copying scripts to ${HOME}/sls-scripts"
+  mkdir -p "${HOME}/${COPY_SCRIPTS_DIR}"
+  cp -R /usr/local/bin/* "${HOME}/${COPY_SCRIPTS_DIR}/."
 }
 
 if [ "$COPY_SCRIPTS_ONLY" = "true" ]; then
-    copy_files
-    exit 0
+  copy_files
+  exit 0
 fi
 
 if [ "$COPY_SCRIPTS" = "true" ]; then
-    copy_files
+  copy_files
 fi
 sh -c "$*"
