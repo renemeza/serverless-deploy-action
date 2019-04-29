2e2bc7789c6944aac45c8f78d9b604b6b0198e87
Rene Meza <rene.meza125@gmail.com>
2019-04-29 01:52:24 -0600

    Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 COPY_SCRIPTS_ONLY=${COPY_SCRIPTS_ONLY:-"false"}
 
-if [ COPY_SCRIPTS_ONLY = "true" ]; then
+if [ "$COPY_SCRIPTS_ONLY" = "true" ]; then
     echo "### Copying scripts to ${HOME}/scripts/bin"
     cp -R /sls-scripts "${HOME}"/.
 else
