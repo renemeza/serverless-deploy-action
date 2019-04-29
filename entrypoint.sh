4d857888b28edddac8d3bc35b4e3f343cb98230b
Rene Meza <rene.meza125@gmail.com>
2019-04-29 01:37:11 -0600

    Add log for copying files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 COPY_SCRIPTS_ONLY=${COPY_SCRIPTS_ONLY:-"false"}
 
 if [ COPY_SCRIPTS_ONLY = "true" ]; then
+    echo "### Copying scripts to ${HOME}/scripts/bin"
     cp -R /scripts/bin "${HOME}"/.
 else
     sh -c "$*"
